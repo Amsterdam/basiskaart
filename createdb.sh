1097e8e91efc6874feb2504c69cc93da88bc249a
Stephan Zaat <s.zaat@amsterdam.nl>
2016-11-17 11:38:59 +0100

    TG-468 Automate BGT / Texts

--- createdb.sh
+++ createdb.sh
@@ -7,3 +7,4 @@ chmod 600 ~/.pgpass
 createdb -h ${DATABASE_PORT_5432_TCP_ADDR} -U ${DB_USER} ${DB_NAME}
 psql -h ${DATABASE_PORT_5432_TCP_ADDR} -U ${DB_USER} -d basiskaart -c "create schema kbk10"
 psql -h ${DATABASE_PORT_5432_TCP_ADDR} -U ${DB_USER} -d basiskaart -c "create schema kbk50"
+psql -h ${DATABASE_PORT_5432_TCP_ADDR} -U ${DB_USER} -d basiskaart -c "create schema bgt"
