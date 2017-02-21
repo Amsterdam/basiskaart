e6eb22657862a77e155240234344045e4e798a4d
dick <dick.kniep@lindix.nl>
2017-02-21 10:07:15 +0100

    #673 vanuit xls genereren van views voor bgt

--- data_import/basiskaart/hoogteligging.py
+++ data_import/basiskaart/hoogteligging.py
@@ -1,20 +1,61 @@
 from openpyxl import load_workbook
-import os
 
-VIEWS = '../fixtures/20170216_wms_kaart_database.xlsx'
+from sql_utils import SQLRunner
+
+XLS_VIEWDEF = '../fixtures/20170216_wms_kaart_database.xlsx'
 
 
 def create_views_based_on_workbook():
-    work_list = []
-    wb = load_workbook(VIEWS)
+    view_definitions = read_workbook()
+    min_max_values = get_min_max_value(view_definitions)
+    create_view(view_definitions, min_max_values)
+
+
+def read_workbook():
+    view_definitions = {}
+    wb = load_workbook(XLS_VIEWDEF)
     startvalue = 1
     for idx, row in enumerate(wb['Blad1'].rows):
+        rowvalues = [r.value for r in row]
+        schema, tabel, categorie, geotype, viewnm, vwattr, laag, grp, minhoogte, maxhoogte = rowvalues
         if idx >= startvalue:
-            viewname = row[4]
-            if viewname in work_list:
-                work_list[viewname],append(row)
-            else:
-                work_list[viewname] = [row]
+            viewname = "{}.{}_{}<hoogteligging>".format(schema.lower(), categorie, geotype)
+            if viewname not in view_definitions:
+                view_definitions[viewname] = []
+            view_definitions[viewname] += [[schema.lower(), tabel, vwattr, minhoogte, maxhoogte]]
+    return view_definitions
 
-    for viewname, viewdef in work_list.items():
-        print(r)
+
+def get_min_max_value(view_definitions):
+    min_max_values = {}
+    for viewname, viewdef in view_definitions.items():
+        for viewrow in viewdef:
+            minvalue = viewrow[3]
+            maxvalue = viewrow[4]
+            if viewname not in min_max_values:
+                min_max_values[viewname] = [0, 0]
+            if min_max_values[viewname][0] > minvalue:
+                min_max_values[viewname][0] = minvalue
+            if min_max_values[viewname][1] < maxvalue:
+                min_max_values[viewname][1] = maxvalue
+    return min_max_values
+
+
+def create_view(view_definitions, min_max_values):
+    sql = SQLRunner()
+
+    viewstmt = 'CREATE OR REPLACE VIEW "{}" AS {}'
+    single_select = 'SELECT {} FROM "{}"."{}" WHERE hoogteligging = {}'
+
+    for viewname, viewdef in view_definitions.items():
+        minvalue = min_max_values[viewname][0]
+        maxvalue = min_max_values[viewname][1]
+
+        for hoogte in range(minvalue, maxvalue):
+            selects = []
+
+            for schema, tabel, vwattr, minval, maxval in viewdef:
+                selects.append(single_select.format(vwattr, schema, tabel, hoogte))
+
+            real_viewname = viewname.replace('<hoogteligging>', str(hoogte).replace('-','_'))
+            sql.run_sql(viewstmt.format(real_viewname, " UNION ".join(selects)))
--- data_import/basiskaart/tests/test_hoogteligging.py
+++ data_import/basiskaart/tests/test_hoogteligging.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
-import basiskaart_setup as bs
+import basiskaart_setup
 from basiskaart import fill_bk
-from hoogteligging import create_views_based_on_workbook
+import hoogteligging
 
-
-# Geen testen voor retrieval van files vanaf objectstore. Bestaande code....
-VALUES = bs.VALUES
+VIEWPATH = os.path.dirname(os.path.realpath(__file__)) + '/fixtures/views'
+hoogteligging.XLS_VIEWDEF = os.path.join(VIEWPATH, '20170216_wms_kaart_database.xlsx')
+VALUES = basiskaart_setup.VALUES
 
 def test_hoogteview():
     tmpdir = VALUES['bgt'][0][1]
@@ -17,4 +17,4 @@ def test_hoogteview():
         tmpdir)
     fill_bk(tmpdir, 'bgt')
 
-    create_views_based_on_workbook()
+    hoogteligging.create_views_based_on_workbook()
