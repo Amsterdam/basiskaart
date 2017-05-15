bad9df546395440eac95b1b0075b61d5acc0947e
Thijs Creemers <thijs.creemers@gmail.com>
2017-05-15 09:23:47 +0200

    disable old test

--- src/tests/test_hoogteligging.py
+++ src/tests/test_hoogteligging.py
@@ -14,21 +14,21 @@ basiskaart.hoogteligging.XLS_VIEWDEF = os.path.join(VIEWPATH, 'wms_kaart_databas
 VALUES = basiskaart_setup.VALUES
 
 
-def test_hoogteview():
-    tmpdir = VALUES['bgt'][0][1]
-    shutil.rmtree(tmpdir, ignore_errors=True)
-    shutil.copytree(
-        os.path.dirname(os.path.realpath(__file__)) + '/fixtures/views',
-        tmpdir + '/1')
-    fill_basiskaart(tmpdir, 'bgt', 1)
-
-    create_views_based_on_workbook()
-    sql = sql_utils.SQLRunner()
-    for hoogte in ('_2', '_1', '0', '1', '2'):
-        exists = sql.run_sql("select exists(select count(*) from bgt.spoor_lijn{})".format(hoogte))
-        print('hoogte {} exists {}'.format(hoogte, exists))
-        print(exists)
-        assert (exists[0][0])
+# def test_hoogteview():
+#     tmpdir = VALUES['bgt'][0][1]
+#     shutil.rmtree(tmpdir, ignore_errors=True)
+#     shutil.copytree(
+#         os.path.dirname(os.path.realpath(__file__)) + '/fixtures/views',
+#         tmpdir + '/1')
+#     fill_basiskaart(tmpdir, 'bgt', 1)
+#
+#     create_views_based_on_workbook()
+#     sql = sql_utils.SQLRunner()
+#     for hoogte in ('_2', '_1', '0', '1', '2'):
+#         exists = sql.run_sql("select exists(select count(*) from bgt.spoor_lijn{})".format(hoogte))
+#         print('hoogte {} exists {}'.format(hoogte, exists))
+#         print(exists)
+#         assert (exists[0][0])
 
 # def test_create_indexes():
 #     tmpdir = VALUES['bgt'][0][1]
